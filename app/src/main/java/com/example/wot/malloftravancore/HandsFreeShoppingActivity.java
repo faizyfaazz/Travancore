877227eca5fce305a4d09e1c094598d0682e34cd
faizyfaazz <faizyfaazz@gmail.com>
2017-09-28 13:04:50 +0530

    first commit

---
+++ app/src/main/java/com/example/wot/malloftravancore/HandsFreeShoppingActivity.java
@@ -0,0 +1,19 @@
+package com.example.wot.malloftravancore;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HandsFreeShoppingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hands_free_shopping);
+    }
+
+}
